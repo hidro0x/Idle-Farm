1aa23d13c2922d0bce0e896249808c692c46ea42
Yiğit Durmuş <ygtdrms05@gmail.com>
2025-03-17 14:40:20 +0300

    Basic Building

--- Efsun Case Study/Assets/Game/Building/Building.cs
+++ Efsun Case Study/Assets/Game/Building/Building.cs
@@ -1,9 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
-public class Building : MonoBehaviour
+
+public class Building : SerializedScriptableObject
 {
+    [SerializeField] private int id;
+    
+    [SerializeField] private int baseCapacity;
+    [SerializeField] private float baseProductionTime;
+    [SerializeField] private int productionRequirementAmount;
+    [SerializeField] private int productionOutputAmount;
+
+    public int Capacity => baseCapacity;
+    public float ProductionTime => baseProductionTime;
+    public int RequiredAmount => productionRequirementAmount;
+    public int OutputAmount => productionOutputAmount;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +28,18 @@ public class Building : MonoBehaviour
     void Update()
     {
         
+    }
+}
+
+[System.Serializable]
+public struct BuildingData
+{
+    public int CurrentCapacity { get; private set; }
+    public float TimeLeft { get; private set; }
+
+    public BuildingData(int currentCapacity, float timeLeft)
+    {
+        CurrentCapacity = currentCapacity;
+        TimeLeft = timeLeft;
     }
 }
